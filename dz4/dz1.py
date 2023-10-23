59bb83d361525a04f17f5af1909d557403be1a79
FoxViki <white-kitten-63@mail.ru>
2023-10-23 21:16:53 +0400

    poslednii test

--- dz4/dz1.py
+++ dz4/dz1.py
@@ -18,3 +18,31 @@
 # 3 5
 
 
+var1 = '5 4'
+var2 = '10 20 30 40 50'
+var3 = '10 20 30 40 50'
+#var2 = var2.replace(' ', '')
+#var3 = var3.replace(' ', '')
+print(var2)
+print(var3)
+set1 = set(var2)
+set2 = set(var3)
+print(set1)
+print(set2)
+set1.remove(' ')
+set2.remove(' ')
+print(set1)
+print(set2)
+set_ob = sorted(set1.intersection(set2))
+my_list = list(set_ob)
+result = " ".join(my_list)
+print(result)
+
+#set1 = set(map(int, var2.split()))
+#set2 = set(map(int, var3.split()))
+# set3 = set1.intersection(set2)
+#print(len(set1))
+#print(len(set2))
+# print(a)
+# print(b) 
+
