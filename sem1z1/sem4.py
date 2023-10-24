d4b267fe6d62003211e05117dc5237466a650f24
FoxViki <white-kitten-63@mail.ru>
2023-10-25 00:51:27 +0400

    hhuh

--- sem1z1/sem4.py
+++ sem1z1/sem4.py
@@ -77,43 +77,76 @@
 # через пробел. Если таких холодильников нет, ничего
 # выводить не нужно.
 
-# Формат входных данных
-# В первой строке подаётся число 
-# n
-# n – количество холодильников. В последующих 
-# n
-# n строках вводятся строки, содержащие латинские строчные
-# буквы и цифры, в каждой строке от 
-# 5
-# 5 до 
-# 100
-# 100 символов.
-
-# Формат выходных данных
-# Программа должна вывести номера зараженных холодильников
-# через пробел. Если таких холодильников нет, ничего выводить не нужно.
 
 
-# Sample Input 1:
-# 6
-# 222anton456
-# a1n1t1o1n1
-# 0000a0000n00t00000o000000n
-# gylfole
-# richard
-# ant0n
-# Sample Output 1:
-# 1 2 3
-# Sample Input 2:
-# 9
-# osfjwoiergwoignaewpjofwoeijfnwfonewfoignewtowenffnoeiwowjfninoiwfen
-# anton
-# aoooooooooontooooo
-# elelelelelelelelelel
-# ntoneeee
-# tonee
-# 253235235a5323352n25235352t253523523235oo235523523523n
-# antoooooooooooooooooooooooooooooooooooooooooooooooooooon
-# unton
-# Sample Output 2:
-# 1 2 7 8
+s1 = '6'
+s2 = '222anton456'
+s3 = 'a1n1t1o1n1'
+s4 = '0000a0000n00t00000o000000n'
+s5 = 'gylfole'
+s6 = 'richard'
+s7 = 'ant0n'
+s8 = '9'
+s9 = 'osfjwoiergwoignaewpjofwoeijfnwfonewfoignewtowenffnoeiwowjfninoiwfen'
+s10 = 'c'
+s11 = 'aoooooooooontooooo'
+s12 = 'elelelelelelelelelel'
+s13 = 'ntoneeee'
+s14 = 'tonee'
+s15 = '253235235a5323352n25235352t253523523235oo235523523523n'
+s16 = 'antoooooooooooooooooooooooooooooooooooooooooooooooooooon'
+s17 = 'unton'
+
+k = 'anton'
+s = k.split()
+
+dict = {s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17}
+for i in s:
+    a = 0
+    n = 0
+    t = 0
+    o = 0
+    n = 0
+    for j in dict:
+        if j == 'a':
+            a == True
+            y = j
+            if a == True & i == 'n':
+                n == True
+                if a == True & n == True & i == 't':
+                    t == True
+                    if a == True & n == True & t == True & i == 't':
+                        o == True
+                        if a == True & n == True & t == True & o == True & i == 't':
+                            n == True
+                            print('Заражен')
+    print(f'Заражен этот холодидьник {dict[j]}')
+                        
+        
+#         print("Найден вирус 'anton' at position", position)
+#     else:
+#         print("Вирусов нет", position)
+#     dict = dict.upper()
+    
+    
+     
+# r=[] #список с номерами результатов
+# n = int(input()) 
+# for i in range(n):
+#     a,n,t,o,nn=0,0,0,0,0 #устанавливаем что все буквы не найдены
+#     s = str(input()) 
+#     for j in s:
+#        if j =='a':
+#             a = True
+#        if j='n' and a: #если найдена буква n и до этого была найдена буква а
+#              n=True #то помечаем букву n как найденую
+#        if j='t' and n:
+#               t=True
+#        if j='o' and t:
+#              o=True
+#        if j='n' and o: #если найдена последняя буква n
+#              r.append(i) #добавляем номер в список результатов
+#               print('virus') 
+#               break; #завершаем поиск по строке, так как цель достигнута
+# if res == 'anton':
+#         print(i, end=' ')
