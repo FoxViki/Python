c24a14da22254a739bc11e6f37b841c6c420799f
FoxViki <white-kitten-63@mail.ru>
2023-11-14 12:32:26 +0400

    jjfjfj

--- sem2grup/sem2z2.py
+++ sem2grup/sem2z2.py
@@ -2,7 +2,40 @@
 # каким по счету числом Фибоначчи оно является,
 # то есть выведите такое число n, что φ(n)=A.
 # Если А не является числом Фибоначчи, выведите число -1.
-
 # Input:     5
-
 # Output:  6
+
+a = input("Input a number: ")
+
+while not a.isdigit() or a == "0" or a == "1":
+    print("Error input")
+    a = input("Input a number: ")
+
+a = int(a)
+
+pos = 3
+pred = 1
+pred2 = 1
+while pred < a:
+    pred, pred2 = pred + pred2, pred     #множественное присваивание одновременно
+    pos += 1
+
+if pred != a:
+    print(-1)
+else:
+    print(pos)
+    
+    
+    
+    
+    
+    
+#     fjgfcfchcmghvv,gvv
+# pos = 3
+# pred = 1
+# pred2 = 1
+# while pred < a:
+#     temp=pred
+#     pred= pred + pred2
+#     pred2=temp
+#     pos += 1
