90027b44a35842722f1e622ad35687e0b552e685
FoxViki <white-kitten-63@mail.ru>
2023-11-16 20:52:04 +0400

    kdshshd

--- dz6/dz2.py
+++ dz6/dz2.py
@@ -7,12 +7,12 @@
 
 # На входе:
 
-a1 = 2
-d = 3
-n = 4
+# a1 = 2
+# d = 3
+# n = 4
 
-ap=list(n)
-print(type(ap))
+# ap=list(n)
+# print(type(ap))
 # list_arr[0]=a1
 # list_arr.append(list_arr[0])
 # for i in range(1, n+1):
@@ -25,3 +25,11 @@ print(type(ap))
 #     a=
 # a = int(input()) # пользователь вводит целое число
 # list_1.append(a)
+a1 = 7
+d = 2
+n = 5
+arr=[]
+for i in range(n):
+    c = a1 + i * d
+    arr.append(c)
+    print(c)
