0a8bd640ebeb25ddb396f3ff0d8a33d8eaf57431
FoxViki <white-kitten-63@mail.ru>
2023-11-24 14:51:36 +0400

    hjh

--- sem2grup/sem5z3.py
+++ sem2grup/sem5z3.py
@@ -28,3 +28,29 @@ while not num_check(num):
 
 num = int(num)
 print('YES' if simple_num(num) else 'NO')
+
+
+
+
+#2 var
+def simple_num(number):
+    if number % 2 == 0 or number % 3 == 0 and number not in (2,3):
+        return False
+    for div in range(3, int(number ** 0.5) + 1, 2):
+        if number % div == 0:
+            return False
+    return True
+
+def num_check(number):
+    if number.isdigit() and int(number) > 1:
+        return True
+    return False
+
+num = input('Enter a number: ')
+
+while not num_check(num):
+    print('Incorrect number!')
+    num = input('Enter a number: ')
+
+num = int(num)
+print('YES' if simple_num(num) else 'NO')
