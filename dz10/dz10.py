10fc2d1336df7f1870055d86cb3b5c7c6c31e6f9
FoxViki <white-kitten-63@mail.ru>
2023-11-30 00:50:38 +0400

    kkfjbvb

--- dz10/dz10.py
+++ dz10/dz10.py
@@ -7,11 +7,11 @@ lst += ['human'] * 10
 random.shuffle(lst) # перемешивание
 print(lst)
 
-data = pd.DataFrame({'whoAmI':lst})
-data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
-data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
-data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
-data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+data = pd.DataFrame({'whoAmI':lst})#добавляем колонки и выводим значения в ячейки на пересечении хуайэма и колонки
+data.loc[data['whoAmI'] == 'robot', 'robot_str'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot_str'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human_str'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human_str'] = '0'
 
 data.head(n=20) 
-data.drop(columns='whoAmI')#удаление столбца
+data.drop(columns='whoAmI')#удаление столбца хуайэм
